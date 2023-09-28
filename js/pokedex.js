37d4458be676fb787a284eba3b1baf6db11b25ae
Roberto Rebolledo <robertorebolledo151@gmail.com>
2023-09-28 19:12:57 +0200

    fondo en el modal-body molon y sinamico, cambia según el tipo de pokemon que sea

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+
     $.ajax({
         url: 'https://pokeapi.co/api/v2/pokemon?limit=1010&offset=0',
         type: 'GET'
@@ -10,51 +11,54 @@ $(document).ready(() => {
             var template = `
             <div class="col-12 col-md-3 col-xl-2 col-xxl-1 card btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" personajeid="${i}">
                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png" class="card-img" alt="...">
-                <div class="">
-                    <h5 class="card-title text-center text-white">${i}</h5>
-                </div>
-            </div>
-            `;
-            $('#pokeList').append(template);
-            i++;
+                </div>`;
+                $('#pokeList').append(template);
+                i++;
+            });
         });
-    });
-
-    $(document).on('click', '.card', function () {
-        var personajeid = $(this).attr('personajeid');
-        $.ajax({
-            url: 'https://pokeapi.co/api/v2/pokemon/' + personajeid,
-            type: 'GET'
-        }).done(function (poke) {
-            $('.pokesprite').attr('src', poke.sprites.front_default);
-            $('#name').text(poke.species.name.toUpperCase());
-            $('#height').text(poke.height);
-            $('#weight').text(poke.weight);
-            $('#type1').text(poke.types[0].type.name);
-
-            $('#type1').parent().addClass('pkm-type');
-            $('#type1').parent().addClass(poke.types[0].type.name);
-
-            if (poke.types[1]) {
-                $('#type2').text((poke.types[1].type.name));
-                $('#type2').parent().addClass('pkm-type');
-                $('#type2').parent().addClass(poke.types[1].type.name);
-
-            } else {
-                $('#type2').text('');
-
-            }
-            $('#hp').text(poke.stats[0].base_stat);
-            $('#attack').text(poke.stats[1].base_stat);
-            $('#defense').text(poke.stats[2].base_stat);
-            $('#special-attack').text(poke.stats[3].base_stat);
-            $('#special-defense').text(poke.stats[4].base_stat);
-            $('#hp').text(poke.stats[5].base_stat);
-            $('#hp').text(poke.stats[6].base_stat);
+        $(document).on('click', '*', function () {
+            $('#type1').parent().removeClass();
+            $('#type2').parent().removeClass(); 
+            if($('pkm-type'))
+            $('.pkm-type').removeClass();
+        })
         });
-    });
-    $(document).on('click', '.btn-close', function () {
-        $('#type1').parent().removeClass();
-        $('#type2').parent().removeClass();
-    })
+        $(document).on('click', '.card', function () {
+            var personajeid = $(this).attr('personajeid');
+            $.ajax({
+                url: 'https://pokeapi.co/api/v2/pokemon/' + personajeid,
+                type: 'GET'
+            }).done(function (poke) {
+                var type1 = poke.types[0].type.name
+                $('.pokesprite').attr('src', poke.sprites.front_default);
+                $('#name').text(poke.species.name.toUpperCase());
+                $('#height').text(poke.height);
+                $('#weight').text(poke.weight);
+                $('#pokedex').text(poke.id);
+                $('#ability').text(poke.abilities[0].ability.name);
+                $('#type1').text(type1);
+                $('#type1').parent().addClass('pkm-type');
+                $('#type1').parent().addClass(type1);
+                var color_temporal1 = $('#type1').parent().css("background-color");
+                var bgcolor;
+                if (poke.types[1]) {
+                    var type2 = poke.types[1].type.name;
+                    $('#type2').text(type2);
+                    $('#type2').parent().addClass('pkm-type');
+                    $('#type2').parent().addClass(type2);
+                    var color_temporal2 = $('#type2').parent().css("background-color");
+                    bgcolor = 'linear-gradient(338deg, '+color_temporal2+' 0%, '+color_temporal1+' 100%)';
+                } else {
+                    $('#type2').parent().removeClass(); 
+                    $('#type2').text('');
+                    bgcolor = 'linear-gradient(338deg, '+color_temporal1+' 0%, '+color_temporal1+' 100%)';
+                }
+                $('.modal-content').css("background",bgcolor);
+                $('#hp').text(poke.stats[0].base_stat);
+                $('#attack').text(poke.stats[1].base_stat);
+                $('#defense').text(poke.stats[2].base_stat);
+                $('#special-attack').text(poke.stats[3].base_stat);
+                $('#special-defense').text(poke.stats[4].base_stat);
+                $('#speed').text(poke.stats[5].base_stat);
+            });
 });
