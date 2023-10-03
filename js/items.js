9fb0135986acee3b082735dcc1691f0212a6d6bd
mgaangel10 <mg.aangel10@gmail.com>
2023-10-03 22:48:12 +0200

    solo falta la descripcion

--- js/items.js
+++ js/items.js
@@ -17,10 +17,11 @@
         });
     });
 */
+
 $(document).ready(function(){
 
     $(document).on('click','#poke',function(){
-
+        $('#nombre').empty();
         $.ajax({
             url: 'https://pokeapi.co/api/v2/item-pocket/1/',
             type:'GET'
@@ -34,18 +35,25 @@ $(document).ready(function(){
                     }).done(function(resp){
                         var guardarItem = resp.items;
                         for (let j = 0; j < guardarItem.length; j++) {
-                          console.log(guardarItem[j]);
-                                        
+                          
+                            $.ajax({
+                                url:guardarItem[j].url,
+                                type:'GET'
+                            }).done(function(name){
+                                var cd= name.effect_entries;
+                                for (let w = 0; w < cd.length; w++) {
+                                    console.log(cd[w].short_effect)
+                                    var template = `<tr class="Justify-content-bettwen">
+                                    <td class="d-flex justify-content-between" >
+                                    <span title="${cd[w].short_effect}">${guardarItem[j].name}</span>
+                                    <span>${name.cost}¥</span>
+                                    </td>
+                                    </tr>`;
+                                    $('#nombre').append(template);
+                                }
+                            })
 
-                          var template = `<tr>
-                          <td >
-                          <span>Nombre</span>
-        <span >${guardarItem[j].name}</span>
-                          </td>
-
-
-                      </tr>`;
-                          $('#nombre').append(template);
+                         
                             
                         }
                     })
@@ -58,7 +66,51 @@ $(document).ready(function(){
            
                        
         })
+    
+   
+        $(document).on('click','#bici',function(){
+            $('#nombre').empty();
+            $.ajax({
+                url:'https://pokeapi.co/api/v2/item-pocket/8/',
+                type:'GET'
+            }).done(function(itCat){
+                var saveIt=itCat.categories;
+                for (var t = 0; t < saveIt.length; t++) {
+                   $.ajax({
+                    url:saveIt[t].url,
+                    type:'GET'
+                   }).done(function(cat){
+                    var saveCat = cat.items;
+                    for (let s = 0; s < saveCat.length; s++) {
+                        console.log(saveCat[s].url)
+                        $.ajax({
+                            url:saveCat[s].url,
+                            type:'GET'
+                        }).done(function(cost){
+                            var template = `<tr class="Justify-content-bettwen">
+                            <td class="d-flex justify-content-between" >
+                            
+                            <span >${saveCat[s].name}</span>
+                            <span >${cost.cost}¥</span>
+                            </td>
+  
+  
+                        </tr>`;
+                            $('#nombre').append(template);
+                        })
+                        
+                    }
+                   })
+                    
+                }
+            })
+        })
     })
+
+       
+
+
+   
     
 
     
